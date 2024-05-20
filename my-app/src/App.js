e755f1cbcb128143a7d5990d74493fd3ca14f570
Yakovlev Denis <yakovlevdk39@gmail.com>
2024-05-20 20:15:20 +0300

    Без JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,60 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 // Всё декларативный
+
+import { createElement } from 'react';
+
 export const App = () => {
 	const currentDate = new Date();
 	const currentYear = currentDate.getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				className: 'App-logo',
+				src: logo,
+				alt: 'logo',
+			}),
+			createElement('p', null, 'Edit src/App.js and save to reload.'),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', null, currentYear),
+		),
 	);
 };
-
-// import logo from './logo.svg';
-// import './App.css';
-// import { createElement } from 'react';
-
-// export const App = () => {
-// 	const currentDate = new Date();
-// 	const currentYear = currentDate.getFullYear();
-// 	return createElement(
-// 		'div',
-// 		{ className: 'App' },
-// 		createElement(
-// 			'header',
-// 			{ className: 'App-header' },
-// 			createElement('img', {
-// 				className: 'App-logo',
-// 				src: logo,
-// 				alt: 'logo',
-// 			}),
-// 			createElement('p', null, 'Edit src/App.js and save to reload.'),
-// 			createElement(
-// 				'a',
-// 				{
-// 					className: 'App-link',
-// 					href: 'https://reactjs.org',
-// 					target: '_blank',
-// 					rel: 'noopener noreferrer',
-// 				},
-// 				'Learn React',
-// 			),
-// 			createElement('p', null, currentYear),
-// 		),
-// 	);
-// };
